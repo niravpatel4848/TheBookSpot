a16829d734fe19f896b324af8e73d243885a94da
Neerav Patel <niravpatel4848@gmail.com>
2022-03-01 00:24:05 -0600

    Added edit and delete methods for Category, added client side validation and fontawesome

--- TheBookSpot/Controllers/CategoryController.cs
+++ TheBookSpot/Controllers/CategoryController.cs
@@ -20,19 +20,100 @@ namespace TheBookSpot.Controllers
             return View(catList);
         }
 
+        //get method for adding genre
         public IActionResult Create()
         {
 
             return View();
         }
 
+        //post method for adding genre
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category category)
         {
-            _db.Categories.Add(category);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        //get method to edit genre
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var obj = _db.Categories.Find(id);
+                if (obj == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(obj);
+                }
+            }
+        }
+
+        //post method to edit genre
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        //get method for delete genre
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var obj = _db.Categories.Find(id);
+                if (obj == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(obj);
+                }
+            }
+        }
+
+        //post method to delete genre
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
         }
     }
 }
