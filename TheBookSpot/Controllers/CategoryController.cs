726c7e5fb8d22792f7cbd8d29eeb9bdf89125807
Neerav Patel <niravpatel4848@gmail.com>
2022-02-28 19:56:30 -0600

    Add project files.

---
+++ TheBookSpot/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TheBookSpot.Data;
+using TheBookSpot.Models;
+
+namespace TheBookSpot.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> catList = _db.Categories;
+
+            return View(catList);
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ TheBookSpot/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheBookSpot.Models;
+
+namespace TheBookSpot.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
